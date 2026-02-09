37bc763606bb57438221e15eefae2a45c5afe06d
Paulo Salvatore <salvatoregames@gmail.com>
2026-02-09 23:28:29 +0000

    Adiciona rota para obter personagem por ID e arquivo de requisição correspondente

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ app.get('/personagens', (req, res) => {
   res.send(lista)
 })
 
+app.get('/personagens/:id', (req, res) => {
+  const id = req.params.id
+
+  const personagem = lista[id - 1]
+
+  res.send(personagem)
+})
+
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
 })
